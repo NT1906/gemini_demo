69fcf4e02cf13648fb21f63f6f5ff700147ecc5d
Nisarg Trivedi <69729785+NT1906@users.noreply.github.com>
2024-02-26 21:54:57 -0800

    Add files via upload

---
+++ gemini_demo.py
@@ -0,0 +1,30 @@
+import PIL.Image
+import streamlit as st
+import google.generativeai as genai
+import pyperclip
+
+st.title("Image to text generator")
+st.divider()
+
+
+uploaded_file = st.file_uploader("Choose a file")
+
+
+if uploaded_file is not None:
+    # Read the uploaded file as an image
+    img = PIL.Image.open(uploaded_file)
+    st.image(uploaded_file)
+    # Setup your API key
+    GOOGLE_API_KEY = 'AIzaSyD7kzLfv9uOyk2yojcso0Lk7ofbmd8p3oI'  # Replace with your actual Google API key
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+    # Use the gemini-pro model for text generation
+    model = genai.GenerativeModel('gemini-pro-vision')
+
+    # Generate text from the converted image text
+    response = model.generate_content(img)
+    st.write(response.text)
+    
+
+   
+    
